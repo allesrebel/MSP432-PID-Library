0810790541d93a85bc1879e583c40377d62ef261
Alles Rebel <allesrebel@yahoo.com>
2015-05-16 02:17:01 -0700

    Code Compiles and Interrupts work!
    *Fixed Buggy Interrupts by keeping C style table
    *Fixed ISRs to use C style symbols

--- example_main.cpp
+++ example_main.cpp
@@ -2,6 +2,12 @@
 #include "MyDriverLib/mydriverlib.h"
 #include "PID_v1.h"
 
+//Define Variables we'll be connecting to
+double Setpoint, Input, Output;
+
+//Specify the links and initial tuning parameters
+PID myPID(&Input, &Output, &Setpoint,2,5,1, DIRECT);
+
 void main(void){
     WDTCTL = WDTPW | WDTHOLD;           // Stop watchdog timer
     setup_clock();
@@ -9,5 +15,14 @@ void main(void){
 
     __enable_interrupts();
 
-    while(1);
+
+    Input = 0;
+    Setpoint = 100;
+
+    myPID.SetMode(AUTOMATIC);
+
+    while(1){
+    	Input = 0;
+    	myPID.Compute();
+    }
 }
